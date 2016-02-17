b93c4b545dcf80c71c8ece4f03cc1a4f969a4af6
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-02-17 15:29:01 -0800

    Homework 2/17, functional

---
+++ hoursinyear.rb
@@ -0,0 +1,16 @@
+#hours year
+# min decade
+# age in seconds
+#seconds to years
+
+require 'date'
+
+yearhours = 365 * 24
+yearmins = yearhours * 60
+yearsecs = yearmins * 60
+jeremyyears = 1136086041/(60*60*24*365)
+
+puts "In one year there are #{yearhours} hours per year}"
+puts "In a decade, there are #{10*yearmins}"
+puts "Nicole Iwasaki is #{Time.now.to_i - Date.new(1992,7,9).to_time.to_i} seconds old"
+puts "Jeremy is #{jeremyyears} years old"
